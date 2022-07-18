b5dec15275d59fa253b20bd8da400b47adbe7bc2
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2022-07-17 21:37:50 -0400

    creating repository script

--- 02-rentalx/src/routes/categories.routes.ts
+++ 02-rentalx/src/routes/categories.routes.ts
@@ -1,24 +1,28 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../respositories/CategoryRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "category already exists" });
+  }
 
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
-  return response.status(201).json();
+  return response.status(201).send();
 });
+
+categoriesRoutes.get("/", (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.status(200).json(categories);
+});
+
 export { categoriesRoutes };
